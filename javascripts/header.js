37cb341b54c033f383c7c1ae34907e37c652b907
Gan Xu <luptior@gmail.com>
2018-09-03 01:23:13 -0500

    change to share common header and footer
    use javascript

---
+++ javascripts/header.js
@@ -0,0 +1,15 @@
+document.writeln("<header>");
+document.writeln("<center><img class=\'img-circle\' src=\'./resources/icon.jpg\' style=\'width:150px;height:150px;\' title=\'Portrait\'></center>");
+document.writeln("<br/>");
+document.writeln("<h2><center>徐甘 Gan Xu</center></h2>");
+document.writeln("    ");
+document.writeln("<info>Graduate Student @</info><br/>");
+document.writeln("<info>Computer Science and Engineering</info><br/>");
+document.writeln("<info>Washington University in St. Louis</info><br/>");
+document.writeln("    <br/>");
+document.writeln("<info>Research Interest:<br/>Bioinformatics, Machine Learning</info><br/>");
+document.writeln("<br/>");
+document.writeln("<info>Previous:</info><br/>");
+document.writeln("<info>Assisstant Bioinformatician @</info><br/>");
+document.writeln("<info>Jones\' Lab, UNC Chapel Hill</info>");
+document.writeln("</header>");
